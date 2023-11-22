ac2ec52c402c71257dc9e489672bbbc5bf653e42
Ahmad Hanif <ahmadhanif2222@gmail.com>
2023-11-22 23:38:59 +0500

    Commit 1 22nd Nov: Completed front end of addproduct

--- Fashion Front/client/src/Components/AddProduct/AddProduct.js
+++ Fashion Front/client/src/Components/AddProduct/AddProduct.js
@@ -1,38 +1,90 @@
 import React from 'react'
-
-import { Form, Button } from 'react-bootstrap'
+import './AddProduct.css'
+import { Row, Col, Container, Form, Button } from 'react-bootstrap'
 
 function AddProduct() {
+    const [product, setProduct] = React.useState({
+        productName: '',
+        productDescription: '',
+        productPrice: 0,
+        productImage: [],
+        productBrand: '',
+        productCategory: '',
+        productStock: 0
+    });
+
+    const handleChange = (e) => {
+        const { name, value, type, files } = e.target;
+        if (type === 'file') {
+            // Handle file input separately to support multiple files
+            const fileArray = Array.from(files);
+            setProduct((prev) => ({
+                ...prev,
+                [name]: fileArray,
+            }));
+        } else {
+            // Handle other input types
+            setProduct((prev) => ({
+                ...prev,
+                [name]: value,
+            }));
+        }
+        
+    }
+
     return (
-        <div>
-            {/* Form to get data regarding the new product being launched by the seller */}
-            <Form>
 
-                <Form.Group controlId="productImage">
-                    <Form.Label>Product Image</Form.Label>
-                    <Form.Control type="file" />
-                </Form.Group>
-                <Form.Group controlId="productName">
-                    <Form.Label>Product Name</Form.Label>
-                    <Form.Control type="text" placeholder="Enter product name" />
-                </Form.Group>
+        <Col>
+            <Row className='customtitle'>
+                <h3>Add Product</h3>
+            </Row>
+            <Row className='customCol'>
 
-                <Form.Group controlId="productDescription">
-                    <Form.Label>Product Description</Form.Label>
-                    <Form.Control as="textarea" rows={3} placeholder="Enter product description" />
-                </Form.Group>
+                <Form>
 
-                <Form.Group controlId="productPrice">
-                    <Form.Label>Product Price</Form.Label>
-                    <Form.Control type="number" placeholder="Enter product price" />
-                </Form.Group>
+                    <Form.Group controlId="productImage" className='customforms'>
+                        <Form.Label>Product Image</Form.Label>
+                        <Form.Control name='productImage' onChange={handleChange} className='customform' type="file" multiple accept=".jpg, .jpeg, .png" />
+                        <Form.Label>Product Name</Form.Label>
+                        <Form.Control name='productName' value={product.productName} onChange={handleChange} className='customform' type="text" placeholder="Enter Product name" />
+                        <Form.Label>Product Brand</Form.Label>
+                        <Form.Control name='productBrand' value={product.productBrand} onChange={handleChange} className='customform' type="text" placeholder="Enter Product Brand" />
+                        <Form.Label>Product Description</Form.Label>
+                        <Form.Control name='productDescription' value={product.productDescription} onChange={handleChange} as="textarea" className='customform' rows={3} placeholder="Enter product description" />
+                        <Form.Label>Product Category</Form.Label>
 
-                <Button variant="primary" type="submit">
-                    Add Product
-                </Button>
-            </Form>
+                        <Form.Select name='productCategory' value={product.productCategory} onChange={handleChange} className='customform'>
+                            <option value="men">Men</option>
+                            <option value="women">Women</option>
+                            <option value="kids">Kids</option>
+                            <option value="minor-fault">Minor Fault</option>
+                            <option value="clearance">Clearance</option>
+                        </Form.Select>
+                        <Form.Label>Product Stock</Form.Label>
+                        <Form.Control name='productStock' value={product.productStock} onChange={handleChange} className='customform' type="number" placeholder="Enter product Stock" />
+                        <Form.Label>Product Price</Form.Label>
+                        <Form.Control name='productPrice' value={product.productPrice} onChange={handleChange} className='customform' type="number" placeholder="Enter product price" />
+                    </Form.Group>
+                    {/* <Form.Group controlId="productName">
+                       
+                    </Form.Group>
 
-        </div>
+                    <Form.Group controlId="productDescription">
+                       
+                    </Form.Group>
+
+                    <Form.Group controlId="productPrice">
+                      
+                    </Form.Group> */}
+
+                    <Button className='custombutton1' variant="primary" type="submit">
+                        Add Product
+                    </Button>
+                </Form>
+            </Row>
+                    {console.log(product)}
+        </Col>
+
     )
 }
 
