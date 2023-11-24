ad5846f47f883967a43b996b50619877e0f121ca
Ahmad Hanif <ahmadhanif2222@gmail.com>
2023-11-24 23:04:32 +0500

    Commit 3 24th Nov : Completed homepage with carousel

--- Fashion Front/client/src/Components/Header/Header.js
+++ Fashion Front/client/src/Components/Header/Header.js
@@ -1,51 +1,52 @@
-import React from 'react';
+import React, {useState}from 'react';
 import logo from '../../logo.svg';
 import cart from './Icons/cart.png';
 import find from './Icons/find.png';
 import profile from './Icons/profile.png';
 import close from './Icons/close.png';
+import Cookies from 'js-cookie';
+import './Header.css';
 import { Navbar, Nav, NavDropdown, Container, Form, Button } from 'react-bootstrap';
 
-class Header extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            isActive: false,
-        };
+function Header(props) {
+    const [isActive, setIsActive] = useState(false);
+    const openSearch = () => {
+        setIsActive(true);
+    };
+    const closeSearch = () => {
+        setIsActive(false);
+    };
+    
+    const logout = () => {
+        Cookies.remove('token');
+        window.location.href = '/login';
     }
     
-    openSearch = () => {
-        this.setState({ isActive: true });
-    };
-    closeSearch = () => {
-        this.setState({ isActive: false });
-    };
-    render() {
-        return (
-            <div>
-                <div className={`search-bar ${this.state.isActive ? 'search-bar-active' : ''}`}>
+    return (
+        <div>
+            <div className={`search-bar ${isActive ? 'search-bar-active' : ''}`}>
                     <div className='container'>
                         <Form className="d-flex ">
                             <Form.Control
-                            type="search"
-                            placeholder="Search"
-                            className="me-2 custom-search no-outine"
-                            aria-label="Search"
+                                type="search"
+                                placeholder="Search"
+                                className="me-2 custom-search no-outine"
+                                aria-label="Search"
                             />
-                            <Button variant="outline-success nav-icons" className='nav-icons'><img alt = "search icon" src={find}/></Button>
-                            <Button variant="outline-success nav-icons" className='nav-icons' onClick={this.closeSearch}><img alt = "close icon" src={close}/></Button>
+                            <Button variant="outline-success nav-icons" className='nav-icons'><img alt="search icon" src={find} /></Button>
+                            <Button variant="outline-success nav-icons" className='nav-icons' onClick={closeSearch}><img alt="close icon" src={close} /></Button>
                         </Form>
                     </div>
                 </div>
-                <Navbar bg="light"  expand="lg" className="customNav">
+                <Navbar bg="light" expand="lg" className="customNav">
                     <Container>
                         <Navbar.Brand href="/">
                             <img
-                            src={logo}
-                            width="50"
-                            height="50"
-                            className="d-inline-block align-top"
-                            alt="Ecommerce Logo"
+                                src={logo}
+                                width="50"
+                                height="50"
+                                className="d-inline-block align-top logo"
+                                alt="Ecommerce Logo"
                             />
                         </Navbar.Brand>
                         <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -62,6 +63,7 @@ class Header extends React.Component {
                                     <NavDropdown.Item href="#action/3.3">Kids</NavDropdown.Item>
                                 </NavDropdown>
                                 <Nav.Link href="/contact">Contact</Nav.Link>
+                                <Nav.Link href="/addseller">Become a seller</Nav.Link>
                                 <Nav.Link href="/aboutUs">About Us</Nav.Link>
                             </Nav>
                             <Nav>
@@ -74,19 +76,34 @@ class Header extends React.Component {
                                     /> 
                                     <Button variant="outline-success search-icon nav-icons" onClick={this.openSearch}><img alt = "search icon" src={find}/></Button>*/}
                                 </Form>
-                                
-                                <Nav.Link href="#search" className='nav-icons search-icon' onClick={this.openSearch}><img alt = "search icon" src={find}/></Nav.Link>
-                                <Nav.Link href='/login' className='nav-icons'><img src={profile}/></Nav.Link>
-                                <Nav.Link href="/cart" className= 'nav-icons'><img src={cart}/></Nav.Link>
-                                
+
+                                <Nav.Link href="#search" className='nav-icons search-icon' onClick={openSearch}><img alt="search icon" src={find} /></Nav.Link>
+                                <NavDropdown className='nav-icons user-icon' title={<img src={profile} alt='profile-icon' />} id="basic-nav-dropdown">
+                                    {
+                                        props.user ?
+                                            <div>
+                                                <NavDropdown.Item href="/profile">Profile</NavDropdown.Item>
+                                                <NavDropdown.Item onClick={logout}>Logout</NavDropdown.Item>
+                                            </div>
+                                            :
+                                            <div>
+                                                <NavDropdown.Item href="/login">Login</NavDropdown.Item>
+                                                <NavDropdown.Item href="/register">Register</NavDropdown.Item>
+                                            </div>
+                                    }
+                                    {/* <NavDropdown.Item href="#action/3.1">Men</NavDropdown.Item>
+                                    <NavDropdown.Item href="#action/3.2">Women</NavDropdown.Item>
+                                    <NavDropdown.Item href="#action/3.3">Kids</NavDropdown.Item> */}
+                                </NavDropdown>
+                                {/* <Nav.Link href='/login' className='nav-icons'><img src={profile} /></Nav.Link> */}
+                                <Nav.Link href="/cart" className='nav-icons'><img src={cart} alt='cart-icon' /></Nav.Link>
                             </Nav>
-                            
                         </Navbar.Collapse>
                     </Container>
                 </Navbar>
-            </div>
-        );
-    }
+        </div>
+    )
 }
 
 export default Header;
+
--- Fashion Front/client/src/Components/Home/Home.js
+++ Fashion Front/client/src/Components/Home/Home.js
@@ -1,15 +1,29 @@
 import React from 'react';
-// import { Form, Button, Container, Row, Col } from 'react-bootstrap';
+import './Home.css';
+import { Form, Button, Container, Row, Col, Carousel } from 'react-bootstrap';
 
 class Home extends React.Component {
     render() {
         return (
-            
             <div>
-                <h1>Home Page</h1>
-            </div>
+                <div className='home-background'>
+                    <Carousel>
+                        <Carousel.Item>
+                            <img src='http://localhost:8080/img/products/home1.jpg' text="First slide" />
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img src='http://localhost:8080/img/products/home2.jpg' text="Second slide" />
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img src='http://localhost:8080/img/products/home3.jpg' text="Third slide" />
+                        </Carousel.Item>
+                    </Carousel>
+                </div>
+                
+            </div >
+
         );
     }
 }
 
-export default Home;
+export default Home;    
